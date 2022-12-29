10caff2c36520d7de3767d32645c36d93c7f0587
Felix Wieland <mail@felixwie.com>
2022-12-29 09:48:22 +0100

    feat: removes azure function for cluster control
    unused/not working functions for cluster management are removed

--- azure/control/dailyStopCluster/run.ps1
+++
@@ -1,7 +0,0 @@
-# Input bindings are passed in via param block.
-param($Timer)
-
-Write-Host "Sending the cluster to sleep..."
-Set-AzContext -Subscription $env:ARM_SUBSCRIPTION_ID
-Stop-AzAksCluster -Name cluster -ResourceGroupName cluster -NoWait
-Write-Host "Successfully sent the cluster to sleep!"
