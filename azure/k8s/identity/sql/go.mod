b64b00074a6a3bd140c740ee91b2c7ec6a5333b0
Felix Wieland <mail@felixwie.com>
2022-12-31 16:26:09 +0100

    feat(identity): adds sample container for sql
    sample workload for connection to an azure sql database using the pod identity

---
+++ azure/k8s/identity/sql/go.mod
@@ -0,0 +1,23 @@
+module felixwie.com/identity/storage
+
+go 1.19
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.1.4
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.6.1
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.1 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ azure/k8s/identity/sql/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	mssql "github.com/microsoft/go-mssqldb"
+)
+
+func main() {
+	clientID := os.Getenv("AZURE_CLIENT_ID")
+	tenantID := os.Getenv("AZURE_TENANT_ID")
+	tokenFilePath := os.Getenv("AZURE_FEDERATED_TOKEN_FILE")
+
+	var connectionString = fmt.Sprintf("server=%s;user id=%s; database=%s", "url", "client id", "db")
+
+	connection, err := mssql.NewConnectorWithAccessTokenProvider(connectionString, func(ctx context.Context) (string, error) {
+		credential, err := azidentity.NewClientAssertionCredential(tenantID, clientID, func(ctx context.Context) (string, error) {
+			content, err := os.ReadFile(tokenFilePath)
+			if err != nil {
+				return "", err
+			}
+
+			return string(content), nil
+		}, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		token, err := credential.GetToken(ctx, policy.TokenRequestOptions{
+			Scopes: []string{"https://database.windows.net/.default"},
+		})
+		if err != nil {
+			return "", err
+		}
+
+		return token.Token, nil
+	})
+
+	if err != nil {
+		log.Fatalf("could not create access token provider: %v", err)
+	}
+	db := sql.OpenDB(connection)
+
+	if err = db.PingContext(context.Background()); err != nil {
+		log.Fatalf("could not ping database: %v", err)
+	}
+	log.Println("connected")
+}
